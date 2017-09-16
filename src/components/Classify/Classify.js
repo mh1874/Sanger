2e7b666cc4e55258e74a81d2aadeea2c46e18eee
mh1874 <mh1874@sina.com>
2017-09-16 19:05:59 +0800

    更正首页的全屏滚动 以及分类页的菜单横向滚动

--- src/components/Classify/Classify.js
+++ src/components/Classify/Classify.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import Header from '../Header/Header.js';
 import Footer from '../Footer/Footer.js';
-
+//引入betterScroll
+import BScroll from 'better-scroll'
 
 export default class Classify extends React.Component {
+	componentDidMount() {
+		this._menuScroll();
+	}
+	_menuScroll() {
+		new BScroll(this.refs.classifyWrapper, {
+			click: true,
+			scrollX: true
+		})
+	}
 	render() {
 		return (
 			<div className="classify">
 					<Header />
-					<ul className="classify-ul">
-						<li>蛋糕</li>
-						<li>冰淇淋</li>
-						<li>咖啡</li>
-						<li>设计师礼品</li>
-	        			<li>企业专区</li>
-					</ul>
+					<div className="classifyWrapper" ref="classifyWrapper">
+						<ul className="classify-ul">
+							<li>蛋糕</li>
+							<li>冰淇淋</li>
+							<li>咖啡</li>
+							<li>设计师礼品</li>
+		        			<li>企业专区</li>
+						</ul>
+					</div>	
 					<div className="container"><h1>内容</h1></div>
 					<Footer />
 			</div>
