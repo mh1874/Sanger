f03c38f825d6979d66867d9263914e4ef924817e
shutiaonn <648779567@qq.com>
2017-09-16 22:36:37 +0800

    bug修改完善

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -43,48 +43,45 @@ class Home extends React.Component {
 	}
 	render() {
 		return (
-			<div id="homeMain" >
-				<div className="home">
-					<Header />
-					<div className="section" ref="homeWrapper">
-						<div className="homeWrapper">	
-							<Carousel autoplay={4000}>
-							    <div><h3>1</h3></div>
-							    <div><h3>2</h3></div>
-							</Carousel>
-							<div className="menu">
-								<ul>
-									<li>
-										<div>1</div>
-										<span>蛋糕</span>
-									</li>
-									<li>
-										<div>2</div>
-										<span>冰淇淋</span>
-									</li>
-									<li>
-										<div>3</div>
-										<span>咖啡</span>
-									</li>
-										<li>
-										<div>4</div>
-										<span>企业专区</span>
-									</li>
-								</ul>
+				<div id="homeMain" >
+					<div className="home">
+						<Header />
+							<div className="section" ref="homeWrapper">
+								<div className="homeWrapper">	
+									<Carousel autoplay={4000}>
+									    {
+											this.state.swiper.map((item,index)=>{
+												return <div key={"xx"+index} className="swiperImg">
+															<img src={item.src} />
+													   </div>
+											})
+										}
+									</Carousel>
+									<div className="menu">
+										<ul>
+											{
+												this.state.list.map((item, index) => {
+													return <li key={item._id}>
+																<div><img src={item.src} /></div>
+																<span>{item.name}</span>
+															</li>
+												})
+											}
+										</ul>
+									</div>
+									<ul className="home-ul">
+										{
+											this.state.list.map((item, index) => {
+												return <li key={item._id}>
+														<img src={item.src} alt="item.name"/>
+														<span>{item.name}</span>
+														<Link to={"/detail/" + item.name}>详情页</Link>
+													</li>
+											})
+										}
+									</ul>
+								</div>
 							</div>
-							<ul className="home-ul">
-								{
-									this.state.list.map((item, index) => {
-										return <li key={item._id}>
-												<img src={item.src} alt="item.name"/>
-												<span>{item.name}</span>
-												<Link to={"/detail/" + item.name}>详情页</Link>
-											</li>
-									})
-								}
-							</ul>
-						</div>
-					</div>
 					<Footer />
 				</div>
 			</div>	
