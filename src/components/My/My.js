6626a31dfc7df2db2c1043acf4e028e14aea65b0
XiGua-DongGua <gejian1happy@163.com>
2017-09-18 22:39:15 +0800

    登录页面的布局以及验证码插件的使用

--- src/components/My/My.js
+++ src/components/My/My.js
@@ -1,12 +1,62 @@
 import React from 'react';
 import Footer from '../Footer/Footer.js';
+import { Input } from 'antd';
+//引入验证码插件
+import '../../gVerify.js';
 
 export default class My extends React.Component {
+	constructor(){
+		super()
+	}
+	componentDidMount(){
+		var verifyCode = new window.GVerify("container");
+	}
 	render() {
+		
 		return (
 			<div className="my">
-				<h1>My</h1>
-				<div className="container"></div>
+				<div className="container">
+					<div className="content">
+						<div className="headerLogin">
+							<span>账号密码登陆</span>
+							<span>手机验证码登录</span>
+						</div>
+						<div className="numberLogin">
+							<form method="post" name="">
+								<Input type="text" placeholder="用户名/邮箱地址"/>
+								<Input type="password" placeholder="填写密码"/>
+							</form>
+						</div>
+						<div className="phoneLogin">
+							<Input type="text" placeholder="手机号"/>
+							<div className="SecurityCode">
+								<Input type="text" placeholder="请输入图片字符"/>
+								<div id="container"></div>
+							</div>
+							<div className="getCode">
+								<Input type="text" placeholder="输入手机验证码"/>
+								<div className="getCodeBtn">
+									发送验证码
+								</div>
+							</div>
+							
+						</div>
+						<div className="login">
+							登录
+						</div>
+						<div className="footLogin">
+							<div className="checkbox">
+								<input type="checkbox" name="rememberMe" />记住密码
+							</div>
+							<div className="toRegister">
+								忘记密码
+								&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+								去注册
+							</div>
+						</div>
+					</div>
+					
+				</div>
 				<Footer />
 			</div>
 		);
